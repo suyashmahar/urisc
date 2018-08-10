317637e0fbc1549379eb03271f6ffbb15f3c8fbb
Suyash Mahar <suyash12mah.uec2016@iitr.ac.in>
2018-08-11 01:44:36 +0530

    Fix clk divider

--- src/clockDivider.sv
+++ src/clockDivider.sv
@@ -8,23 +8,26 @@ module clockDivider
   #(
     parameter DIVIDE_BY = 2
     )(
-      input 			   clkIn,
+      input 	 clkIn,
       output reg [DIVIDE_BY - 1:0] clkOut
       );
    
-   reg 				   shiftReg [DIVIDE_BY-1:0];
+   reg [DIVIDE_BY-1:0] 		   shiftReg;
 
    assign clkOut = shiftReg;
+
+   integer 			   count;
    
    initial begin
-       shiftReg = {DIVIDE_BY{1'b0},1'b1};
+       for (count = 0; count < DIVIDE_BY; count++) begin
+	   shiftReg[count] = 0;
+       end
    end
 
    // Shift a register containing a single 1 to generate n clocks,
    // where n is the width of the shift registers
-   always @(posedge clk) begin
+   always @(posedge clkIn) begin
        shiftReg <= {shiftReg[0], shiftReg[DIVIDE_BY-1:1]};
    end
    
 endmodule // clockDivider
-
